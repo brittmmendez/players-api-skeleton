786b03ce5bd93e1665f831ec0f1749338662eeb2
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-17 15:05:52 -0400

    set up user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+//stores the schema for a user
+const UserSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name:{
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  passwordConf: {
+    type: String,
+  }
+});
 
 module.exports = {User}
