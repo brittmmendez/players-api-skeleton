7c14884418fd9ea1f8d5eaeb1a437bc0d9fc3a0d
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-17 16:02:54 -0400

    organized server.js

--- src/server.js
+++ src/server.js
@@ -17,6 +17,52 @@ const port = process.env.PORT                   //sets up local port or heroku p
 
 app.use(bodyParser.json());                     //middleware - takes the body data sent from client json and convert it to an object attaching it on to the request object
 
+// CREATE USER
+app.post('/api/user', (req, res) => {
+  let passwordConf =req.body.passwordConf
+  let body = _.pick(req.body, ['first_name', 'last_name', 'email', 'password']);
+
+  if (body.password !== passwordConf) {                //confirm password
+    return res.status(400).send('Error: Password and confirmation password must match');
+  }
+
+  let user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();                       //call the method to geenrate token
+  }).then((token) => {
+    res.header('x-auth', token).send(user);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+// GET /users/:id
+app.get('/api/users/:id', authenticate, (req, res) => {   //runs middleware authencate and sends response below if no errors
+  res.send(req.user);                                     //sending the user the request with the info we found/set in findByToken
+});
+
+// USER LOGIN
+app.post('/api/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {    //call to veryfy if  user exsists with that email. check password
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
+    });
+  }).catch((e) =>{
+    res.status(400).send();
+  });
+});
+
+//SIGN OUT
+app.delete('/api/logout', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 //CREATE PLAYER
 app.post('/api/players', authenticate, (req, res) => {
 
@@ -121,55 +167,5 @@ app.patch('/api/players/:id', authenticate, (req, res) => {
     res.status(400).send();
   })
 });
-
-// CREATE USER
-app.post('/api/user', (req, res) => {
-  let passwordConf =req.body.passwordConf
-  let body = _.pick(req.body, ['first_name', 'last_name', 'email', 'password']);
-
-  if (body.password !== passwordConf) {                //confirm password
-    return res.status(400).send('Error: Password and confirmation password must match');
-  }
-
-  let user = new User(body);
-  user.save().then(() => {
-    return user.generateAuthToken();                       //call the method to geenrate token
-  }).then((token) => {
-    res.header('x-auth', token).send(user);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
-  }).catch((e) => {
-    res.status(400).send(e);
-  })
-});
-
-// GET /users/:id
-app.get('/api/users/:id', authenticate, (req, res) => {   //runs middleware authencate and sends response below if no errors
-  res.send(req.user);                                     //sending the user the request with the info we found/set in findByToken
-});
-
-// USER LOGIN
-app.post('/api/login', (req, res) => {
-  let body = _.pick(req.body, ['email', 'password']);
-
-  User.findByCredentials(body.email, body.password).then((user) => {    //call to veryfy if  user exsists with that email. check password
-    user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
-    });
-  }).catch((e) =>{
-    res.status(400).send();
-  });
-});
-
-//SIGN OUT
-app.delete('/api/logout', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
-    res.status(200).send();
-  }, () => {
-    res.status(400).send();
-  });
-});
-
-app.listen(3000, () => {
-  console.log(`Started on port ${port}`);
-})
 
 module.exports = {app};
