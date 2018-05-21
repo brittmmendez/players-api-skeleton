cc7259f3a81677e30df3d7dd484e7921ea6f912f
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-21 17:08:22 -0400

    all tests passinging

--- src/server.js
+++ src/server.js
@@ -12,95 +12,97 @@ mongoose.connect('mongodb://localhost:27017/PingPong');
 const Player = require('./models/player');
 const User = require('./models/user');
 const {authenticate} = require('./middleware/authenticate');
+const UserController = require('./controllers/UserController');
+
 
 const app = express();                          //stores the express application
 
 app.use(bodyParser.json());                     //middleware - takes the body data sent from client json and convert it to an object attaching it on to the request object
 
-// CREATE USER
-app.post('/api/user', (req, res) => {
-  let confirm_password =req.body.confirm_password
-  let body = _.pick(req.body, ['first_name', 'last_name', 'email', 'password']);
-
-  if (body.password !== confirm_password) {                //confirm password
-     let err = new Error('Passwords do not match.');
-     res.status(409).send(err);
-  } else {
-    let user = new User(body);
-    user.save().then(() => {
-      return user.generateAuthToken();                       //call the method to geenrate token
-    }).then((token) => {
-      let body = {
-      success: true,
-      token: token,
-      user: {
-        id: user._id
-      }
-    };
-      res.status(201).header('x-auth', token).send(body);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
-    }).catch((e) => {
-      res.status(409).send(e);
-    })
-  };
-});
-
-// USER LOGIN
-app.post('/api/login', (req, res) => {
-  let body = _.pick(req.body, ['email', 'password']);
-
-  User.findByCredentials(body.email, body.password).then((user) => {    //call to veryfy if  user exsists with that email. check password
-    user.generateAuthToken().then((token) => {
-      let body = {
-      success: true,
-      token: token,
-      user: {
-        id: user._id
-      }
-    };
-      res.status(200).send(body);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
-    });
-  }).catch((e) =>{
-    res.status(401).send();
-  });
-});
-
-//UPDATE User
-app.put('/api/user/:userId', (req, res) => {
-  User.findByIdAndUpdate({_id: req.params.userId}, req.body, {new: true})
-  .then((user) => {
-    if (!user) {
-      res.status(500).send();
-    }
-
-    let body = {
-      success: true,
-      user: {
-        first_name: user.first_name,
-        last_name: user.last_name,
-        id: user._id
-      }
-    }
-
-    res.status(200).send(body);
-  }).catch((e) => {
-    res.status(400).send();
-  })
-});
-
-// // GET /users/:id
-// app.get('/api/users/:id', authenticate, (req, res) => {   //runs middleware authencate and sends response below if no errors
-//   res.send(req.user);                                     //sending the user the request with the info we found/set in findByToken
+// // CREATE USER
+// app.post('/api/user', (req, res) => {
+//   let confirm_password =req.body.confirm_password
+//   let body = _.pick(req.body, ['first_name', 'last_name', 'email', 'password']);
+//
+//   if (body.password !== confirm_password) {                //confirm password
+//      let err = new Error('Passwords do not match.');
+//      res.status(409).send(err);
+//   } else {
+//     let user = new User(body);
+//     user.save().then(() => {
+//       return user.generateAuthToken();                       //call the method to geenrate token
+//     }).then((token) => {
+//       let body = {
+//       success: true,
+//       token: token,
+//       user: {
+//         id: user._id
+//       }
+//     };
+//       res.status(201).header('x-auth', token).send(body);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
+//     }).catch((e) => {
+//       res.status(409).send(e);
+//     })
+//   };
 // });
-
-
-// //SIGN OUT
-// app.delete('/api/logout', authenticate, (req, res) => {
-//   req.user.removeToken(req.token).then(() => {
-//     res.status(201).send({success: true});
-//   }, () => {
-//     res.status(400).send();
+//
+// // USER LOGIN
+// app.post('/api/login', (req, res) => {
+//   let body = _.pick(req.body, ['email', 'password']);
+//
+//   User.findByCredentials(body.email, body.password).then((user) => {    //call to veryfy if  user exsists with that email. check password
+//     user.generateAuthToken().then((token) => {
+//       let body = {
+//       success: true,
+//       token: token,
+//       user: {
+//         id: user._id
+//       }
+//     };
+//       res.status(200).send(body);               //send the token back as an http header. x-auth is a custom header for our specific purpose.
+//     });
+//   }).catch((e) =>{
+//     res.status(401).send();
 //   });
 // });
+//
+// //UPDATE User
+// app.put('/api/user/:userId', (req, res) => {
+//   User.findByIdAndUpdate({_id: req.params.userId}, req.body, {new: true})
+//   .then((user) => {
+//     if (!user) {
+//       res.status(500).send();
+//     }
+//
+//     let body = {
+//       success: true,
+//       user: {
+//         first_name: user.first_name,
+//         last_name: user.last_name,
+//         id: user._id
+//       }
+//     }
+//
+//     res.status(200).send(body);
+//   }).catch((e) => {
+//     res.status(400).send();
+//   })
+// });
+//
+// // // GET /users/:id
+// // app.get('/api/users/:id', authenticate, (req, res) => {   //runs middleware authencate and sends response below if no errors
+// //   res.send(req.user);                                     //sending the user the request with the info we found/set in findByToken
+// // });
+//
+//
+// // //SIGN OUT
+// // app.delete('/api/logout', authenticate, (req, res) => {
+// //   req.user.removeToken(req.token).then(() => {
+// //     res.status(201).send({success: true});
+// //   }, () => {
+// //     res.status(400).send();
+// //   });
+// // });
 
 //CREATE PLAYER
 app.post('/api/players', authenticate, (req, res) => {
@@ -219,9 +221,9 @@ app.delete('/api/players/:id', authenticate, (req, res) => {
 //   })
 // });
 
-// app.listen(3000, () => {
-//   console.log("started port");
-// })
+app.listen(3000, () => {
+  console.log("started port");
+})
 
 // module.exports = {app};
 module.exports = app
